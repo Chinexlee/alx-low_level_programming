76f687539147a196ecbadf5beb5e271e1eab04b5
Chinexlee <chinexlee20@gmail.com>
2022-09-09 12:08:43 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,12 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%i is negative\n", n);
+		printf("%i is positive\n", n);
 	}
 	return (0);
-
 }
 
