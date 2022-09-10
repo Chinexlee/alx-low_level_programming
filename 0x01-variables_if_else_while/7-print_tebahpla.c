3f44b69ad849bcebbfde74686275aa932b22c420
Chinexlee <chinexlee20@gmail.com>
2022-09-10 01:35:04 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,7 +5,7 @@
 /**
  * main - a script that print alphabets in reverse
  *
- * Description: using putchar i will be 
+ * Description: using putchar i will be
  * printing alphabets in lowercase in reverse.
  *
  * Return: is always equal to 0
@@ -15,9 +15,8 @@ int main(void)
 {
 	char letters;
 
-	for (letters = 'z'; letters >= 'a' ;letters--)
+	for (letters = 'z'; letters >= 'a' ; letters--)
 		putchar(letters);
-	
 	putchar('\n');
 
 	return (0);
