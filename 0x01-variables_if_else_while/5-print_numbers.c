50c19a9f0ad5889045bd107d4391d710bc6d44e3
Chinexlee <chinexlee20@gmail.com>
2022-09-10 00:33:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include < stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers
+ * of base 10 starting from 0
+ *
+ * Description: a program that prints all single digit numbers of base 
+ * 10 starting
+ *
+ * Return: always equals to 0
+ */
+
+main int(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf('\n');
+	return (0);
+}
