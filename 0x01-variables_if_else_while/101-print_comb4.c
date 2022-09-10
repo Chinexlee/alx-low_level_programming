711275ac408fba934f625df43221acd30971e13a
Chinexlee <chinexlee20@gmail.com>
2022-09-10 15:50:58 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints all possible different
+ * combinations of three digits
+ *
+ * Description: Numbers must be separated by ,
+ * followed by a space
+ * 012, 120, 102, 021, 201, 210 are considered the
+ * same combination of the three digits 0, 1 and 2
+ *
+ * Return: always equals to 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (k = j + 1; k < 58; k++)
+		{
+			for (j = i + 1; j < 57; j++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+		
+			}		
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
