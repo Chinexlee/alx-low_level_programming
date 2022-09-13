5ab2db1046a49b943ddab501634344a1d6d467ac
Chinexlee <chinexlee20@gmail.com>
2022-09-13 15:31:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
 /**
  * main - a function that prints the alphabet, in lowercase,
  * followed by a new line.
+ *
  * Description: a function that prints the alphabet, in lowercase,
  * followed by a new line.
  * Return: always equals to zero
@@ -11,7 +12,7 @@
 void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 
