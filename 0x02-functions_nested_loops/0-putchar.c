cf77a6f2f83c7817f30bdcbc02a999641be82a57
Chinexlee <chinexlee20@gmail.com>
2022-09-13 15:14:56 -0700

    last commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - a program that prints _putchar, followed by a new line.
  *
  * Description: a program that prints _putchar, followed by a new line
- * Return : always equals zero
+ * Return: always equals zero
  */
 
 int main(void)
