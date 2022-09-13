1d1774d832ce04a9dad8420f329150fde73c098d
Chinexlee <chinexlee20@gmail.com>
2022-09-13 15:24:20 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet, in lowercase,
+ * followed by a new line.
+ * Description: a function that prints the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: always equals to zero
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
