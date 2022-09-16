36bff434fd6ef704aae4927b1d269f7a72261e7e
Chinexlee <chinexlee20@gmail.com>
2022-09-16 10:58:44 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
