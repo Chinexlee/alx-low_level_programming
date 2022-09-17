4216637c0d6eb90f3b768750df764eca98e56b57
Chinexlee <chinexlee20@gmail.com>
2022-09-16 21:38:33 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,14 @@ int main(void)
 	while (h <= 100)
 	{
 		if (h % 3 == 0 && h % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (h % 5 == 0)
 		{
 			if (h == 100)
-			{
-				printf("Buzz");
-				printf('\n');
-			}
+		{
+			printf("Buzz");
+			printf("\n");
+		}
 			else
 				printf("Buzz ");
 		}
